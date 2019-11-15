277910899cdaf437b60842bd36296ad0fd341f3d
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-15 18:37:37 +0100

    Added DB insertion

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -1,17 +1,59 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wishlist.Helpers;
+using wishlist.Models;
 
 namespace wishlist.Controllers
 {
     public class ManageController : Controller
     {
+        private DBHandler dbcon = new DBHandler();
+        private string error = "";
         // GET: Manage
         public ActionResult Index()
         {
-            return View();
+            return View(new Manage());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string name, string url, string comment)
+        {
+            if (!name.Equals(""))
+            {
+                if (!url.Equals(""))
+                {
+                    try
+                    {
+                        dbcon.openConnection();
+
+                        MySqlCommand cmd = new MySqlCommand();
+                        cmd.Connection = dbcon.connection;
+                        string sql = "INSERT INTO data (name,url,comment) VALUES ('{0}', '{1}', '{2}')";
+                        cmd.CommandText = string.Format(sql, name, url, comment);
+                        cmd.ExecuteNonQuery();
+
+                        dbcon.closeConnection();
+                    }
+                    catch (MySqlException e)
+                    {
+                        error = e.Message;
+                    }
+                }
+                else
+                {
+                    error = "URL can't be empty";
+                }
+            } else
+            {
+                error = "Name can't be empty";
+            }
+
+            return View(new Manage() { error = error });
+
         }
     }
 }
---
+++ Helpers/DBHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace wishlist.Helpers
+{
+    public class DBHandler
+    {
+        public MySqlConnection connection { get; set; }
+
+        public void openConnection()
+        {
+            connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString);
+            connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
