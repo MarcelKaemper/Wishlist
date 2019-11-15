4fd62104c62884beee363c37c755a11ca1774ea9
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-15 19:49:16 +0100

    Loading data from table

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,30 +1,47 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wishlist.Helpers;
+using wishlist.Models;
 
 namespace wishlist.Controllers
 {
     public class HomeController : Controller
     {
+        private DBHandler dbcon = new DBHandler();
+        private string error = "";
         public ActionResult Index()
         {
-            return View();
-        }
+            dbcon.openConnection();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = dbcon.connection;
+            cmd.CommandText = string.Format("SELECT name,url,comment,added_on FROM wishlist.data");
 
-            return View();
-        }
+            MySqlDataReader reader = cmd.ExecuteReader();
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            List<List<string>> data = new List<List<string>>();
 
-            return View();
+            int row = 0;
+            int count = reader.FieldCount;
+
+            while (reader.Read())
+            {
+                List<string> dict = new List<string>();
+                dict.Add(reader.GetValue(0).ToString());
+                dict.Add(reader.GetValue(1).ToString());
+                dict.Add(reader.GetValue(2).ToString());
+                dict.Add(reader.GetValue(3).ToString());
+
+                data.Add(dict);
+            }
+
+            reader.Close();
+
+            return View(new Home() {data=data});
         }
     }
 }
---
+++ Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wishlist.Models
+{
+    public class Home
+    {
+        public string error { get; set; }
+        public List<List<string>> data { get; set; }
+    }
+}
