155f83fd4ad9dfc229e0546bd2d2aaa1d4b11f70
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-16 13:59:56 +0100

    Using class to handle data requests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,37 +11,9 @@ namespace wishlist.Controllers
 {
     public class HomeController : Controller
     {
-        private DBHandler dbcon = new DBHandler();
-        private string error = "";
         public ActionResult Index()
         {
-            dbcon.openConnection();
-
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = dbcon.connection;
-            cmd.CommandText = string.Format("SELECT name,url,comment,added_on FROM wishlist.data");
-
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            List<List<string>> data = new List<List<string>>();
-
-            int row = 0;
-            int count = reader.FieldCount;
-
-            while (reader.Read())
-            {
-                List<string> dict = new List<string>();
-                dict.Add(reader.GetValue(0).ToString());
-                dict.Add(reader.GetValue(1).ToString());
-                dict.Add(reader.GetValue(2).ToString());
-                dict.Add(reader.GetValue(3).ToString());
-
-                data.Add(dict);
-            }
-
-            reader.Close();
-
-            return View(new Home() {data=data});
+            return View(new Home() {data=new GetData().data()});
         }
     }
 }
