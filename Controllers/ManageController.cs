82c5f9e9b8f5c7a60a83bbdef952bba3f39f69a6
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-17 17:23:16 +0100

    Initial commit

---
+++ Controllers/ManageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wishlist_core.Controllers
+{
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
